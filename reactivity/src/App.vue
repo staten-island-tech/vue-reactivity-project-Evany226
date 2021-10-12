fb6d84f868a50f02cd9578fcc0b418ab8abc2433
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-10-12 12:31:42 -0400

    c

---
+++ reactivity/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div class="main-container">
+  <h1 class="lol">Your mom</h1>
+</div>
+
+
+</template>
+
+<script>
+console.log("hello world");
+</script>
+
+<style>
+
+*{
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+.body {
+  padding: 0;
+  margin: 0;
+}
+
+.main-container {
+  background-color: red;
+}
+
+.lol {
+  font-size: 10rem;
+}
+
+</style>
