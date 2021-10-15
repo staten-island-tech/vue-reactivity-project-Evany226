dd4e178a7ef57a593cfccb74b34195ad2fdf38fe
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-10-15 12:28:51 -0400

    c

--- reactivity/src/App.vue
+++ reactivity/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar">
+      <img>
+    </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -14,15 +15,10 @@ export default {
     HelloWorld
   }
 }
+*/
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
