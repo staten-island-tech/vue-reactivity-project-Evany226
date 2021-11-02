a7a3985224a4d5beb2ab8c4239d5ec3d6a78ad05
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-11-02 19:26:38 -0400

    Revert "COMPLETED PROJECT"
    This reverts commit 25ede5fac89b6e06bdfbd5200640e04f1d5016fe.

--- reactivity/src/App.vue
+++ reactivity/src/App.vue
@@ -2,20 +2,12 @@
   <div id="app">
     <nav class="navbar">
       <img class="logo" src="./assets/amazon.jpg">
-      <div class="search-container">
-          <input type="text" id="search-area" placeholder="">
-            <button id="submit-button" type="submit">
-                <i class="fa fa-search"></i>
-            </button>
-      </div>
-      
-
       <div class="cart-button">
           <i class="fas fa-shopping-cart" @click="toggle"></i>
-          <ul class="shopping-list" v-if="active"> 
-                <li id="list-title">Shopping List</li>
-                <li v-for="(shop, index) in shoppingItems" :key="shop.productName">{{shoppingItems[(index)]}}</li>
-          </ul>
+        <ul class="shopping-list" v-if="active"> 
+              <li id="list-title">Shopping List</li>
+              <li>{{shoppingItems}}</li>
+        </ul>
       </div>
     </nav>
 
@@ -60,76 +52,60 @@ export default {
     technology: [
       {
       productId: 1,
-      productName: 'Keyboard',
+      productName: 'Keyboards',
       productImage: "https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
       },
       {
       productId: 2,
-      productName: 'Monitor',
+      productName: 'Monitors',
       productImage: "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     },
     {
       productId: 3,
-      productName: 'Smartphone',
+      productName: 'Smartphones',
       productImage: "https://images.unsplash.com/photo-1530319067432-f2a729c03db5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1089&q=80",
-    },
-    {
-      productId: 4,
-      productName: 'Laptop',
-      productImage: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80",
     },
     ],
 
     clothing: [
     {
-      productId: 5,
+      productId: 4,
       productName: 'Jeans',
       productImage: "https://images.unsplash.com/photo-1624378440847-4a64ee1a889d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1464&q=80",
     },
     {
-      productId: 6,
-      productName: 'T-shirt',
+      productId: 5,
+      productName: 'T-shirts',
       productImage: "https://images.unsplash.com/photo-1620799139507-2a76f79a2f4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80",
     },
     {
-      productId: 7,
+      productId: 6,
       productName: 'Sneakers',
       productImage: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      productId: 8,
-      productName: 'Glasses',
-      productImage: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
     },
     ],
 
     moreClothing: [
     {
-      productId: 9,
+      productId: 7,
       productName: 'Sweater',
       productImage: "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1072&q=80",
     },
     {
-      productId: 10,
+      productId: 8,
       productName: 'Dress Shirt',
       productImage: "https://images.unsplash.com/photo-1603251578711-3290ca1a0187?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
     },
     {
-      productId: 11,
+      productId: 9,
       productName: 'Denim Shorts',
       productImage: "https://images.unsplash.com/photo-1591195853828-11db59a44f6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-    },
-        {
-      productId: 12,
-      productName: 'Puffer Jacket',
-      productImage: "https://media.istockphoto.com/photos/male-coat-isolated-on-the-white-picture-id163208487?b=1&k=20&m=163208487&s=170667a&w=0&h=YyOFKwoWyLSVO-cXE3goQ4el6K8Cvs082gjmUSouvOQ="
     },
     ],
     active: false
   }
   },
   mounted: function(){
-    console.log(this.shoppingItems);
   },
   methods: {
     toggle() { 
@@ -173,11 +149,11 @@ body{
 
 .logo {
   width: 14rem;
-  padding: 0.5rem;
+  padding: 1rem;
 }
 
 .navbar {
-  background-color: #131921;
+  background-color: #0F1111;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -191,7 +167,7 @@ body{
 
 .item-container {
   display: flex;
-  height: 200vh;
+  height: 140vh;
   width: 100%;
   justify-content: space-evenly;
 
@@ -263,31 +239,8 @@ body{
   color: black;
   border-radius: 0.5rem;
   border: none;
+
 }
-
-
-
-.search-container {
-  display: flex;
-}
-
-#search-area {
-  width: 50vw;
-  height: 5vh;
-  font-size: 2rem;
-  padding-left: 1rem;
-}
-
-#submit-button{
-  background-color: orange;
-  
-}
-
-.fa-search {
-  font-size: 2rem;
-  padding: 0.65rem 0.2rem;
-}
-
 
 
 </style>
